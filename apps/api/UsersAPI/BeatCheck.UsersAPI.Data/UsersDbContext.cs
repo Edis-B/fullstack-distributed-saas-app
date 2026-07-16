2ead479a2687caa0bd9a22526b3e750901d150ea
Edis-B <edi.badjakov@gmail.com>
2026-07-17 00:27:07 +0300

    Initialized UserAPI project.

---
+++ apps/api/UsersAPI/BeatCheck.UsersAPI.Data/UsersDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BeatCheck.UsersAPI.Data
+{
+    public class UsersDbContext : IdentityDbContext<IdentityUser>
+    {
+        public UsersDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
